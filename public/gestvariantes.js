d5da406f30535106053c79454684ce802c06194a
AlonsoDo <correoapolosoft.hotmail.com>
2019-05-16 10:19:08 +0200

    subida 0.0.9

--- public/gestvariantes.js
+++ public/gestvariantes.js
@@ -1,8 +1,30 @@
-﻿function test() {
-    alert('test');
-}
+﻿function GestionVariantes(Str) {
 
-function GestionVariante(Result,LugarVariante) {
-    var cCadenaSalida = Result;
-    $('#male').val('male');    
+    // Desglosar informacion
+    var aInfo = Str.split(' ');
+
+    // Mostrar profundidad
+    var nDepth = aInfo.indexOf('depth');
+    if (nDepth > -1) {
+        var cDepth = aInfo[nDepth + 1];
+        $('#profundidad').text(cDepth);
+    }
+
+    // Mostrar lineas multipv
+    var NumeVari = $('#multyPv').val();
+    NumeVari = parseInt(NumeVari);
+    var ExtraDiv = 54;
+    var TotalHeightDiv = ExtraDiv * NumeVari;
+    $('#variantes').empty();
+
+    //Crear divs variantes
+    for (var i = 1; i < NumeVari + 1; i++) {
+        $('#variantes').append('<div id="variante"' + NumeVari.toString() + ' style="height:40px; width:680px; border:2px solid black; margin-top:8px; margin-left:8px;">' +
+            '<label id="variante' + NumeVari.toString() + '">Aki va la variante</label>' +
+            '</div > ');
+        $('#variantes').css('height', TotalHeightDiv + 'px');
+    }
+
+
+
 }
